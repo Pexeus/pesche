8611cbe90f23b13fdbce8f72240481afdf461ef0
samo <samo@it-tf.local>
2020-10-15 10:34:30 +0200

    auth

--- backend/new/index.php
+++ backend/new/index.php
@@ -1,3 +1,10 @@
+<?php
+            session_start();
+            if(!isset($_SESSION["uid"])){
+                header("Location: ../../signin/index.php");
+                exit();
+            }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
